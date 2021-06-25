62e66660c1a69fa69c9818c0ece6f91833872184
Kunlun Chen <kollinchen1@hotmail.com>
2021-06-25 12:13:06 -0700

    likes bug fixes

--- weekTWO/629/likes/script.js
+++ weekTWO/629/likes/script.js
@@ -1,6 +1,7 @@
 var count = 3;
-var h2 = document.querySelector("#likes");
-function increaseLike(){
+var h2;
+function increaseLike1(){
+    h2 = document.querySelector("#likes");
     console.log(h2);
     count++;
     h2.innerHTML = count +" like(s)";
@@ -11,6 +12,7 @@ var counts = [9,12,9];
 var tag = "";
 function increaseLike(a){
     tag = "#likes"+a;
+    console.log(tag);
     h2 = document.querySelector(tag);
     counts[a]++;
     h2.innerHTML = counts[a] +" like(s)";
