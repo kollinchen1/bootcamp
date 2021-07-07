46fc5aa979add91b879be1e974b1a2b70a479eef
Kunlun Chen <kollinchen1@hotmail.com>
2021-07-07 16:22:00 -0700

    users with bankAcc fixed

--- PYTHON v21.1/week3/707/usersWithBankAccounts/user.py
+++ PYTHON v21.1/week3/707/usersWithBankAccounts/user.py
@@ -30,16 +30,19 @@ class BankAccount:
     @classmethod
     def all_info(self,cls):
         print(f"Interest Rate:{cls.int_rate}, Balance:{cls.balance}")
-class user:
+class User:
     def __init__(self,name):
         self.name = name
         self.accounts = {
             'boa':BankAccount(int_rate=0.02, balance=0),
-            'chase':BankAccount(int_rate=0.05, balance=0)
             }
         
     def make_deposit(self, amount,bank):
-        self.accounts[bank].deposit(amount)
+        #error case
+        if bank in self.accounts:
+            self.accounts[bank].deposit(amount)
+        else:
+            print("error, no bank")
         return self
     def make_withdrawal(self, amount,bank):
         self.accounts[bank].withdraw(amount)
@@ -47,9 +50,14 @@ class user:
     def display_user_balance(self,bank):
         self.accounts[bank].display_account_info()
         return self
+    def add_bank_acc(self,name,int_rate, balance):
+        self.accounts[name] = BankAccount(int_rate,balance)
 
-kol = user("Kol")
+kol = User("Kol")
 kol.make_deposit(1000,'boa')
 kol.display_user_balance("boa")
-kol.make_deposit(2000,'chase')
+kol.add_bank_acc("chase",int_rate=0.05, balance=0)
+kol.make_deposit(2000,'chase')  
 kol.display_user_balance("chase")
+
+# 'chase':BankAccount(int_rate=0.05, balance=0)
